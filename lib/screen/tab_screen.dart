d61cdd6fa157eacff3aa743b48764a589ef8daad
sivsnkr <ced17i026@iiitdm.ac.in>
2021-05-11 17:37:47 +0530

    add app drawer to the app bar

--- lib/screen/tab_screen.dart
+++ lib/screen/tab_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import './favourites_screen.dart';
 import './categories_screen.dart';
+import '../widget/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -32,6 +33,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title']),
       ),
+      drawer: MainDrawer(),
       body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
@@ -42,12 +44,12 @@ class _TabsScreenState extends State<TabsScreen> {
         items: [
           BottomNavigationBarItem(
             backgroundColor: Theme.of(context).primaryColor,
-            icon: Icon(Icons.category),
+            icon: const Icon(Icons.category),
             label: 'Categories',
           ),
           BottomNavigationBarItem(
             backgroundColor: Theme.of(context).primaryColor,
-            icon: Icon(Icons.star),
+            icon: const Icon(Icons.star),
             label: 'Favourites',
           ),
         ],
---
+++ lib/widget/main_drawer.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget buildListTile(ctx, handler, title, icon) {
+    return ListTile(
+      leading: Icon(icon),
+      onTap: handler,
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: const EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).accentColor,
+            child: Text(
+              'Cooking up!',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          buildListTile(
+            context,
+            () {},
+            'Meal',
+            Icons.restaurant,
+          ),
+          buildListTile(
+            context,
+            () {},
+            'Filter',
+            Icons.settings,
+          ),
+        ],
+      ),
+    );
+  }
+}
